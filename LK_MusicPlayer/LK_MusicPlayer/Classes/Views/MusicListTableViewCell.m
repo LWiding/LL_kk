3273c9e45b6c67eff6d22b0837cccee0dcf1b36a
likuan <377509050@qq.com>
2017-08-17 11:56:59 +0800

    Revert "修改代码"
    This reverts commit 2c183e9857dbfc5b00dcd4b788ebbc16998b729d.

--- LK_MusicPlayer/LK_MusicPlayer/Classes/Views/MusicListTableViewCell.m
+++ LK_MusicPlayer/LK_MusicPlayer/Classes/Views/MusicListTableViewCell.m
@@ -19,5 +19,13 @@
     self.singNameLab.text=model.artists_name;
 }
 
+<<<<<<< HEAD
+=======
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+>>>>>>> parent of 2c183e9... 修改代码
 
 @end
